f9011200393e2c6c0bdae5e5bf081a742c2f6a0c
m 169780 <rupanshukundal@gmail.com>
2026-04-13 17:45:15 +0530

    api updated baaki sb kuch ....

--- src/app/api/registerUser/route.js
+++ src/app/api/registerUser/route.js
@@ -1,13 +1,29 @@
 import customerModel from "@/models/customer.model";
 import mongoose from "mongoose";
-export default async function POST(req){
+import bcrypt from "bcrypt";
 
-        
-        if(mongoose.connection.readyState !== 1){
-            await mongoose.connect(process.env.MONGODB_URI, {servertimeoutms: 5000});
+export async function POST(req) {
+
+    try {
+        if (mongoose.connection.readyState !== 1) {
+            await mongoose.connect(process.env.MONGODB_URI, { serverSelectionTimeoutMS: 5000 })
         }
+        const body = await req.json()
 
-    
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(body.password, saltRounds);
+        
+        body.password = hashedPassword;
+        
+        const newCustomer = new customerModel(body)
+
+        await newCustomer.save()
+
+        return Response.json({ message: "User Registered Successfully", data: newCustomer }, { status: 201 })
+    }
+    catch (error) {
+        return Response.json({ message: error.message }, { status: 400 })
+    }
 
 
 }
