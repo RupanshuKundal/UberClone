0b3aa2fe14d0b969048828270fe61dd251739cb2
m 169780 <rupanshukundal@gmail.com>
2026-04-17 19:31:31 +0530

    Added a New Component with visibility on login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './page.css';
 import CustomerForm from '../components/customerForm';
+
+import EmployeeForm from '../components/employeeForm';
 const Page = () => {
   return (
     <div className="grid">
       <div className="heading"><h1>Login</h1></div>
-      <CustomerForm></CustomerForm>
+      {/* <CustomerForm></CustomerForm> */}
+      <EmployeeForm></EmployeeForm>
+
       <br></br>
       <div style={{ display: 'flex', gap: '10px' }}>
           <button className="btn" style={{ backgroundColor: "#fcfcfc", color: "black" }}>
